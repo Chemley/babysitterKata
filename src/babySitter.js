4bc0d701c06b1b25e683c413daf363f82a75af42
Chemley <canaowens@gmail.com>
2017-12-10 15:03:53 -0500

    previous test have passed. working on calculating the total cost per each time increment

--- src/babySitter.js
+++ src/babySitter.js
@@ -1,14 +1,17 @@
 export function babySitter(hours) {
-
-  if (hours > 22) {
+  var cost = 0;
+  if (hours > 21 && hours <= 23) {
     return 8;
+    cost ++;
   }
 
-  if (hours > 17) {
+  if (hours >= 17 && hours <= 21) {
     return 12;
   }
 
   if (hours < 4) {
     return 16;
   }
+
+
 }
--- src/babySitter.test.js
+++ src/babySitter.test.js
@@ -13,12 +13,24 @@ describe ("Babysitter Nightly Charge", function() {
   expect(babySitter(2)).toEqual(16);
   });
 
-  it("time is between 5-9pm, pay $12 an hour", function(){
+  it("time is between 5-9pm, pay $12 an hour", function() {
     expect(babySitter(18)).toEqual(12);
   });
 
-  it("time is between 9-12pm, pay $8 an hour", function(){
+  it("time is between 9-12pm, pay $8 an hour", function() {
     expect(babySitter(22)).toEqual(8);
+  });
+
+  it("time is between midnight - 4am, pay $16 an hour", function() {
+    expect(babySitter(3)).toEqual(16);
+  });
+
+  it("time is between 9pm - 12am, & half-hour, pay $8 an hour", function() {
+    expect(babySitter(22.5)).toEqual(8);
+  });
+
+  it("babysitter worked from 5pm - 9pm, total $/hr is 48", function() {
+    expect(babySitter(20)).toEqual(48);
   });
 
 
