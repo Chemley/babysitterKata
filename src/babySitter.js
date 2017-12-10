3015c17e9dad092d6370b36cb4fb9f331bbb288c
Chemley <canaowens@gmail.com>
2017-12-10 14:43:10 -0500

    test passed for babysitter pay at 8 an hr

--- src/babySitter.js
+++ src/babySitter.js
@@ -1,5 +1,11 @@
 export function babySitter(hours) {
+
+  if (hours > 22) {
+    return 8;
+  }
+
   if (hours > 17) {
     return 12;
   }
+
 }
--- src/babySitter.test.js
+++ src/babySitter.test.js
@@ -6,6 +6,12 @@ describe ("Babysitter Nightly Charge", function() {
   });
 
   it("babysitter gets paid $8", function() {
-  expect(babySitter(22)).toEqual(8);
+  expect(babySitter(23)).toEqual(8);
   });
+
+  it("babysitter gets paid $16", function() {
+  expect(babySitter(2)).toEqual(16);
+  });
+
+
 });
